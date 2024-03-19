b7a4b79d4da2b9ff3d4ccc51247c3098726adc43
liuxu <liuxu@zqykj.com>
2024-03-19 22:54:51 +0800

    项目初始化

---
+++ im-connect/src/main/java/com/myim/ConnectApplicationContext.java
@@ -0,0 +1,17 @@
+package com.myim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lx
+ * @date 2024/03/19
+ */
+@SpringBootApplication
+public class ConnectApplicationContext {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectApplicationContext.class, args);
+    }
+}
