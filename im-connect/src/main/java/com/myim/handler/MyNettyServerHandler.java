5035783c5f9d2d9002d3b164544d15054c11673c
lx <1119878190@qq.com>
2024-03-23 12:02:38 +0800

    支持处理http亲求和socket请求

--- im-connect/src/main/java/com/myim/handler/MyNettyServerHandler.java
+++ im-connect/src/main/java/com/myim/handler/MyNettyServerHandler.java
@@ -1,12 +1,29 @@
 package com.myim.handler;
 
-import io.netty.channel.Channel;
+import com.alibaba.fastjson2.JSON;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketServerHandshaker;
+import io.netty.handler.codec.http.websocketx.WebSocketServerHandshakerFactory;
+import io.netty.util.CharsetUtil;
+import lombok.extern.slf4j.Slf4j;
 
-public class MyNettyServerHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
+@Slf4j
+public class MyNettyServerHandler extends SimpleChannelInboundHandler<Object> {
+
+
+    private WebSocketServerHandshaker handshaker;
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
@@ -21,14 +38,98 @@ public class MyNettyServerHandler extends SimpleChannelInboundHandler<TextWebSoc
         System.out.println("channel 不活动状态 关闭通道");
     }
 
-    @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame s) throws Exception {
-        String text = s.text();
-        System.out.println("服务端收到消息：" + text);
-        Channel channel = channelHandlerContext.channel();
 
-        TextWebSocketFrame textWebSocketFrame = new TextWebSocketFrame("服务端收到消息"+text);
-        channel.writeAndFlush(textWebSocketFrame);
-//        channel.writeAndFlush("服务端收到消息");
+    /**
+     * 处理发送的数据
+     * 对http，websocket单独处理
+     *
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (msg instanceof HttpRequest) {// 如果是HTTP请求，进行HTTP操作
+            handleHttpRequest(ctx, (DefaultHttpRequest)msg);
+        } else if (msg instanceof WebSocketFrame) {// 如果是Websocket请求，则进行websocket操作
+            handleWebSocketFrame(ctx, (WebSocketFrame) msg);
+        }
     }
+
+
+    /**
+     * 对http请求的处理
+     */
+    private void handleHttpRequest(ChannelHandlerContext ctx,
+                                   DefaultHttpRequest req) throws Exception {
+        log.info("这里再处理http请求");
+        // 如果 http解码失败 返回错误
+        if (!req.getDecoderResult().isSuccess()) {
+            sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,
+                    HttpResponseStatus.BAD_REQUEST));
+            return;
+        }
+
+        // 如果是 websocket 握手
+        if (("websocket".equals(req.headers().get("Upgrade")))) {
+            WebSocketServerHandshakerFactory wsFactory = new WebSocketServerHandshakerFactory(
+                    "ws://localhost:8080/ws", null, false);
+            handshaker = wsFactory.newHandshaker(req);
+            if (handshaker == null) {
+                WebSocketServerHandshakerFactory
+                        .sendUnsupportedWebSocketVersionResponse(ctx.channel());
+            } else {
+                handshaker.handshake(ctx.channel(), req);
+            }
+            return;
+        }
+        // http请求
+        String uri = req.getUri();
+        Map<String,String> resMap = new HashMap<>();
+        resMap.put("method",req.getMethod().name());
+        resMap.put("uri",uri);
+        String msg = "<html><head><title>test</title></head><body>你的请求为：" + JSON.toJSONString(resMap) +"</body></html>";
+        // 创建http响应
+        FullHttpResponse response = new DefaultFullHttpResponse(
+                HttpVersion.HTTP_1_1,
+                HttpResponseStatus.OK,
+                Unpooled.copiedBuffer(msg, CharsetUtil.UTF_8));
+        // 设置头信息
+        response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/html; charset=UTF-8");
+        // 将html write到客户端
+        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+
+
+    }
+
+    /**
+     * 对socket请求的处理
+     */
+    private void handleWebSocketFrame(ChannelHandlerContext ctx, WebSocketFrame msg) {
+        // 获取客户端传输过来的消息
+        String content = ((TextWebSocketFrame) msg).text();
+        System.out.println("websocket：：：  接受到的数据：" + content);
+        ctx.writeAndFlush(new TextWebSocketFrame("[服务器在]" + LocalDateTime.now() + "接受到消息, 消息为：" + content));
+
+    }
+
+
+    private static void sendHttpResponse(ChannelHandlerContext ctx,
+                                         DefaultHttpRequest req, FullHttpResponse res) {
+        // 返回应答给客户端
+        if (res.getStatus().code() != 200) {
+            ByteBuf buf = Unpooled.copiedBuffer(res.getStatus().toString(),
+                    CharsetUtil.UTF_8);
+            res.content().writeBytes(buf);
+            buf.release();
+            HttpHeaders.setContentLength(res, res.content().readableBytes());
+        }
+
+        // 如果是非Keep-Alive，关闭连接
+        ChannelFuture f = ctx.channel().writeAndFlush(res);
+        if (!HttpHeaders.isKeepAlive(req) || res.getStatus().code() != 200) {
+            f.addListener(ChannelFutureListener.CLOSE);
+        }
+    }
+
 }
--- im-connect/src/main/java/com/myim/initializer/MyNettyServerInitializer.java
+++ im-connect/src/main/java/com/myim/initializer/MyNettyServerInitializer.java
@@ -4,12 +4,10 @@ import com.myim.handler.MyNettyServerHandler;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.codec.LineBasedFrameDecoder;
 import io.netty.handler.codec.http.HttpServerCodec;
-import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
-import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
 
 public class MyNettyServerInitializer extends ChannelInitializer<SocketChannel> {
 
@@ -18,26 +16,25 @@ public class MyNettyServerInitializer extends ChannelInitializer<SocketChannel> 
         ChannelPipeline pipeline = socketChannel.pipeline();
         // 对写大数据流的支持
         // websocket 基于http协议，所以要有http编解码器
-        pipeline.addLast("http-codec", new HttpServerCodec());
+        pipeline.addLast(new HttpServerCodec());
         // 对写大数据流的支持
-//        pipeline.addLast("http-chunked", new ChunkedWriteHandler());
-//        // 几乎在netty中的编程，都会使用到此handler
+        pipeline.addLast(new ChunkedWriteHandler());
+        // //设置单次请求的文件大小上限
 //        pipeline.addLast("aggregator", new HttpObjectAggregator(65535));
+        // 日志打印级别
+        pipeline.addLast(new LoggingHandler(LogLevel.INFO));
         /**
          * websocket 服务器处理的协议，用于指定给客户端连接访问的路由 : /ws
          * 本handler会帮你处理一些繁重的复杂的事
          * 会帮你处理握手动作： handshaking（close, ping, pong） ping + pong = 心跳
          * 对于websocket来讲，都是以frames进行传输的，不同的数据类型对应的frames也不同
          */
-//        pipeline.addLast(new StringDecoder());
-//        pipeline.addLast(new StringEncoder());
-        pipeline.addLast(new LoggingHandler(LogLevel.DEBUG));
         // 客户端连接地址为 ws://localhost:8000/ws
-        pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
+//        pipeline.addLast(new WebSocketServerProtocolHandler("/ws", true));
         // 目前暂定 字符串编解码，后续改为自定义编解码器解决粘包拆包问题
 
         // 添加 LineBasedFrameDecoder 以解决粘包和拆包问题
-        //pipeline.addLast(new LineBasedFrameDecoder(1024));
+//        pipeline.addLast(new MessageDecoder());
 
         pipeline.addLast(new MyNettyServerHandler());
     }
