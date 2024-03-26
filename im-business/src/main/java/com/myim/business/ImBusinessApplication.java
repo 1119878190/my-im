cc35fb92bb8fd890fbf19e9f3620441a1fa52d44
liuxu <liuxu@zqykj.com>
2024-03-26 16:24:08 +0800

    初始化模块im-business

---
+++ im-business/src/main/java/com/myim/business/ImBusinessApplication.java
@@ -0,0 +1,15 @@
+package com.myim.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lx
+ * @date 2024/03/26
+ */
+@SpringBootApplication
+public class ImBusinessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ImBusinessApplication.class, args);
+    }
+}
