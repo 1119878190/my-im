cf063b3284f46fb95f9aa2f5f5515f043d8eaa31
liuxu <liuxu@zqykj.com>
2024-03-26 16:52:05 +0800

    初始化模块im-lbs

---
+++ im-lbs/src/main/java/com/myim/lbs/ImLBSApplication.java
@@ -0,0 +1,16 @@
+package com.myim.lbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lx
+ * @date 2024/03/26
+ */
+@SpringBootApplication
+public class ImLBSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImLBSApplication.class, args);
+    }
+}
