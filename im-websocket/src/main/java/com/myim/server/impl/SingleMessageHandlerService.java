4a238b3b256e1fc6bc78ae73156f804f952ad0fc
liuxu <liuxu@zqykj.com>
2024-04-09 19:42:58 +0800

    单聊消息-思路编写

--- im-websocket/src/main/java/com/myim/server/impl/SingleMessageHandlerService.java
+++ im-websocket/src/main/java/com/myim/server/impl/SingleMessageHandlerService.java
@@ -14,6 +14,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class SingleMessageHandlerService implements MessageHandlerService {
 
+    /**
+     * 单聊消息处理思路
+     * <p>
+     * 1.websocket服务将接收到消息，将消息发送至mq
+     * 2.business业务服务监听mq的消息，对消息进行处理(持久化)
+     * 3.business将消息发送到接受者对应的mq的queue中(queue为im服务的brokerId，通过redis构建的路由层要精准投递到消息接收者连接的websocket服务上)
+     * 4.websocket服务接收到mq中的消息，将消息推送
+     *
+     * @param ctx
+     * @param msg
+     */
     @Override
     public void messageReceiveHandler(ChannelHandlerContext ctx, Message msg) {
 
